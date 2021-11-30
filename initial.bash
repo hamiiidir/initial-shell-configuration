29b11163f53442539f250c6d299d2865bcef51f6
Hamid Abbasnia <hamidabbasnia@yahoo.com>
2021-12-01 00:48:18 +0330

    modified

--- initial.bash
+++ initial.bash
@@ -17,5 +17,6 @@ source ~/.zshrc;
 sudo localectl set-locale LANG=en_IN.UTF-8;
 
 mkdir -p ~/.local/share/fonts;
-cd ~/.local/share/fonts && curl -fLo "Fira Code Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete.ttf
+cd ~/.local/share/fonts && curl -fLo "Fira Code Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete.ttf;
+fc-cache -fv;
                                                                                 
