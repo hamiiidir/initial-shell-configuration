fa2efa3863131080dc75cc3a10ba2fd0c6b44cc9
Hamid Abbasnia <hamidabbasnia@yahoo.com>
2021-12-01 00:45:18 +0330

    modofied

--- initial.bash
+++ initial.bash
@@ -1,18 +1,21 @@
 sudo apt update;
 sudo apt upgrade;
+sudo apt install zsh;
 
-# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 
 sudo sh -c "$(curl -fsSL https://starship.rs/install.sh)";
 
-echo 'eval "$(starship init zsh)"' >> ~/.bashrc;
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc;
 
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting;
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting;
 
-# sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting)/g' ~/.zshrc;
+sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting)/g' ~/.zshrc;
+
+source ~/.zshrc;
 
 sudo localectl set-locale LANG=en_IN.UTF-8;
 
-cd ~/.local/share/fonts && curl -fLo "Fira Code Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete.ttf;
-
-source ~/.bashrc;                                                                                
+mkdir -p ~/.local/share/fonts;
+cd ~/.local/share/fonts && curl -fLo "Fira Code Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete.ttf
+                                                                                
