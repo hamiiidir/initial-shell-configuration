57a6851f9e3f2fc4c6df14339f94d8acbb32f590
Hamid Abbasnia <hamidabbasnia@yahoo.com>
2021-12-01 22:07:05 +0330

    modified

--- initial.bash
+++ initial.bash
@@ -1,13 +1,8 @@
 sudo apt update;
-# sudo apt upgrade;
+
 sudo apt install zsh;
 
 sudo localectl set-locale LANG=en_IN.UTF-8;
-
-# mkdir -p ~/.local/share/fonts;
-# cd ~/.local/share/fonts && curl -fLo "Sauce Code Pro Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete%20Mono.ttf;
-# sudo apt-get install fontconfig-config;
-# fc-cache -fv;
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 
@@ -17,13 +12,11 @@ sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting)/g' ~/.zshrc;
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="af-magic"/g' ~/.zshrc;
 sed -i 's/#ENABLE_CORRECTION="true"/ ENABLE_CORRECTION="true"/g' ~/.zshrc;
 
-# sh -c "$(curl -fsSL https://starship.rs/install.sh)"
-
-# sudo echo 'eval "$(starship init zsh)"' >> ~/.zshrc;
-
 cd;
 
 source .zshrc;
+
+
 
 
                                                                                 
